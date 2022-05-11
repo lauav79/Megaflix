37b38d913eef34661a8c6e864b933954161ff0c3
lauav79 <laura1999va@gmail.com>
2022-05-11 18:46:43 +0200

    creo estructura de persona-usuario,administrador

---
+++ MegaFlixV1.0/src/megaflixv1/pkg0/Personas/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package megaflixv1.pkg0.Personas;
+
+/**
+ *
+ * @author admin
+ */
+public  abstract class Persona {
+    String user;
+    String nombre;
+    int edad;
+    String tipoUsuario;
+    String password;
+    
+    
+    //metodos
+    public void login(){
+        
+    }
+    
+    public void verPerfil(){
+    
+    }
+
+
+    
+}
